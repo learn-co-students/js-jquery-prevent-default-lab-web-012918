0cc77b61dfae76f2f88d79f6cfb69b416d1e42b3
johnvoyage <jmyork762@gmail.com>
2018-03-20 16:21:03 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-  // call functions here
+  // JY
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+    submitButtonClicked(event)
+  })
+  // submitForm();
 });
 
-// define functions here
+// JY
+function submitButtonClicked(event) {
+  let submitTerm = $("#item").val()
+  $('ol').append(`<li>${submitTerm}</li>`)
+}
+
+// function submitForm(){
+//   $('form').on('submit', function(event){
+//     var listItem = $('#item').val();
+//     $('ol').append( '<li>' + listItem + '</li>' );
+//     event.preventDefault();
+//   });
+// }
