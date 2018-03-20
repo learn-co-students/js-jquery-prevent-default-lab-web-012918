b0da88d7708b2f05852a6932b5cc37a8aa13dd7f
porzingod <jasonlim.ny@gmail.com>
2018-03-20 13:20:48 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   // call functions here
+
+  $("form").submit(function() {
+    submitForm(event)
+  })
 });
 
 // define functions here
+function submitForm(event) {
+  let ol = $("ol")
+  let li = $("<li></li>").text($("#item").val())
+  ol.append(li)
+  event.preventDefault()
+}
