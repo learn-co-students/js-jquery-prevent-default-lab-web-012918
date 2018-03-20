72dacc7306ab2e87544ae6f638a54f742aee0605
zitro <bryan@bryanortiz.com>
2018-03-20 13:53:12 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
-});
+  $('form')[0].addEventListener('submit', function(event){
+    event.preventDefault()
+    add()
+  })
 
-// define functions here
+  function add(){
+    let li = document.createElement('li')
+    li.innerHTML = $('#item')[0].value
+    $("#list ol")[0].append(li)
+  }
+
+});
