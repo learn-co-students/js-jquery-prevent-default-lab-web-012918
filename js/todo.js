d49553a286013758cbdd68367683bd8f2b8cabe7
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-20 15:42:04 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
   // call functions here
-  $('form')[0].addEventListener('submit', function(event){
-    event.preventDefault()
-    add()
-  })
+  submitForm();
+})
 
-  function add(){
-    let li = document.createElement('li')
-    li.innerHTML = $('#item')[0].value
-    $("#list ol")[0].append(li)
-  }
+//define functions here
+function submitForm(){
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var li = $('#item').val(); //doesn't like let
+    $('#list ol').append( '<li>' + li + '</li>' );
+    $('#item').val("")
+  });
+}
 
-});
-
-// define functions here
+//instead of .addEventListener() use the jquery .on which can deal with jquery returning an array
+//same for .value should be .val()
+//and .append is the same .append
