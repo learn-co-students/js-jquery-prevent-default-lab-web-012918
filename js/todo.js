9fd325e2c551df0cd71233dd5d45115f73a8fa29
allinthemind66 <r.nilon92@gmail.com>
2018-03-20 13:17:16 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  // call functions here
-});
+  submitForm();
+  });
 
-// define functions here
+  function submitForm(){
+    $('form').on('submit', function(event){
+      var inputValue = $('#item').val();
+      $('ol').append( '<li>' + inputValue + '</li>' );
+      event.preventDefault();
+    });
+  }
+
+
+  
