25e4ca86fcc1604a63ce7550c4d5bfa32a65d73f
gilsunshine <gilsunshine@gmail.com>
2018-03-20 15:02:04 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   // call functions here
+  ToDoList()
 });
 
 // define functions here
+
+function ToDoList() {
+  let list = $('#list ol')
+  $('form').submit(function(event){
+    event.preventDefault()
+    let li = document.createElement('li')
+    li.innerText = $('#item')[0].value
+    list.append(li)
+  })
+}
