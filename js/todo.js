0de53e3fd43908362b86ef38ab4c1b7ee097d927
scott <you@example.comitsmescottb123@gmail.com>
2018-03-21 09:15:13 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   // call functions here
+  const addItemSubmit = document.querySelector('form')
+  addItemSubmit.addEventListener('submit', createItem)
 });
 
-// define functions here
+function createItem() {
+  event.preventDefault()
+  let itemInputField = document.getElementById('item').value
+  let itemLi = document.createElement('li')
+  itemLi.innerText = itemInputField
+  let itemList = document.getElementById('list')
+  itemList.appendChild(itemLi)
+  document.getElementById('item').value = ""
+}
